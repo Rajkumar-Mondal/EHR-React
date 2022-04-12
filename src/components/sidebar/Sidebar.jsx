7df41bfeceed2371a43ed08f107a2d3560403e5b
unknown <ayan2306tika@gmail.com>
2022-04-12 22:41:47 +0530

    new changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import '../../styles/components/sidebar/Sidebar.scss';
-import { FcAbout } from "react-icons/fc";
+import { IoIosAttach } from "react-icons/io";
+import { IoIosAddCircle } from "react-icons/io";
 export default function Sidebar() {
 return (
 <div class="sidebar">
 <h3 id="first-heading">HER</h3>
-<div id="first-img">
-<img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="error"></img>
-</div>
+<img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="error" id="logo" width="120px"></img>
 <div id="add-sidebar-manu">
 <ul>
     <li>
-    <span class="title"> <FcAbout/><a href="https://www.google.com/" class="listSize">Dashboard</a></span>
+    <span class="title"> <IoIosAttach/><a href="https://www.google.com/" class="listSize">Dashboard</a></span>
     </li>
     <li>
-    <span class="title"> <FcAbout/><a href="https://www.google.com/" class="listSize">New Order</a></span>
+    <span class="title"> <IoIosAddCircle/><a href="https://www.google.com/" class="listSize">New Order</a></span>
     </li>
     <li>
     <span class="title"> <FcAbout/><a href="https://www.google.com/" class="listSize">Order History</a></span>
